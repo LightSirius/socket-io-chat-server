01084b30dcbca7cda02b3c0fbb8fc0c55690a726
LightSirius <54878053+LightSirius@users.noreply.github.com>
2022-06-23 17:13:37 +0900

    Add handle

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,9 +1,35 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { Socket } from 'net';
 
-@WebSocketGateway()
+@WebSocketGateway(3005, { transports: ['websocket'], namespace: 'chat' })
 export class EventsGateway {
+  @WebSocketServer()
+  server: Server;
+
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  async handleMessage(client: Socket, data: string) {
+    console.log(data);
+    console.log(Socket);
+    this.server.emit('message', 'server to client : ' + data);
+  }
+
+  afterInit(client) {
+    console.log('after Init');
+    console.log(client);
+  }
+
+  handleDisconnect(client) {
+    console.log(client);
+  }
+
+  handleConnection(server) {
+    console.log('connected');
+    console.log(server);
   }
 }
